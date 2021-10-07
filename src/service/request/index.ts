77c020323e3bf9ef9d9924c0358563bd7bcd1181
gby <nameGBY@163.com>
2021-10-07 17:53:01 +0800

    feat(axios): 定义拦截器

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,19 +1,33 @@
 import axios from 'axios'
 import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import type { InterceptorsHooks, MyAxiosRequestConfig } from './type'
 
+// 统一封装axios
 class MyRequest {
   instance: AxiosInstance // axios 实例
+  interceptors?: InterceptorsHooks // 请求拦截、响应拦截hooks
 
-  constructor(config: AxiosRequestConfig) {
+  constructor(config: MyAxiosRequestConfig) {
     this.instance = axios.create(config)
+    this.interceptors = config.interceptors
+
+    // 设置拦截器
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptors,
+      this.interceptors?.requestInterceptorsCatch
+    )
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptors,
+      this.interceptors?.responseInterceptorsCatch
+    )
   }
 
   request(config: AxiosRequestConfig): Promise<AxiosResponse<never>> {
     return this.instance.request(config)
   }
 
-  get() {}
-  post() {}
+  // get() {}
+  // post() {}
 }
 
 export default MyRequest
---
+++ src/service/request/type.ts
@@ -0,0 +1,16 @@
+import type { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+// 定义拦截器钩子
+
+interface InterceptorsHooks {
+  requestInterceptors?: (config: AxiosRequestConfig) => AxiosRequestConfig
+  requestInterceptorsCatch?: (err: any) => any
+  responseInterceptors?: (config: AxiosResponse) => AxiosResponse
+  responseInterceptorsCatch?: (err: any) => any
+}
+
+interface MyAxiosRequestConfig extends AxiosRequestConfig {
+  interceptors?: InterceptorsHooks
+}
+
+export { MyAxiosRequestConfig, InterceptorsHooks }
