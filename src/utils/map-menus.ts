a34a32741eea38f8b1a57515862712f75a39bf10
gby <nameGBY@163.com>
2021-10-25 21:48:05 +0800

    feat(router): 设置动态路由

--- src/utils/map-menus.ts
+++ src/utils/map-menus.ts
@@ -1,7 +1,7 @@
 import { RouteRecordRaw } from 'vue-router'
 
 export function mapMenusToRouter(userMenus: any[]): RouteRecordRaw[] {
-  const Router: RouteRecordRaw[] = []
+  const Routers: RouteRecordRaw[] = []
 
   // 1.加载默认 所有 routes (按照 router 下 main 的文件夹)
   const AllRoutes: RouteRecordRaw[] = []
@@ -12,7 +12,19 @@ export function mapMenusToRouter(userMenus: any[]): RouteRecordRaw[] {
     AllRoutes.push(ModuleFile.default)
   })
 
+  console.log('AllRoutes: ', AllRoutes)
   // 2.根据菜单获取需要添加的routes
-
-  return []
+  const _recurseGetRouter = (menus: any[]): void => {
+    for (const menu of menus) {
+      // 类型为 2 的时候是需要的
+      if (menu.type == 2) {
+        const router = AllRoutes.find((router) => router.path == menu.url)
+        if (router) Routers.push(router)
+      } else {
+        _recurseGetRouter(menu.children)
+      }
+    }
+  }
+  _recurseGetRouter(userMenus)
+  return Routers
 }
